e2b31b0ebe6b25ef97a8aae38b44f58425f99efb
Drok012 <123266239+Drok012@users.noreply.github.com>
2024-12-02 23:21:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function handleNewPlayer() {
+    alert("New Player functionality coming soon!");
+  }
+  
+  function handleNewGame() {
+    alert("New Game functionality coming soon!");
+  }
+  
+  function handleLoadGame() {
+    alert("Load Game functionality coming soon!");
+  }
+  
